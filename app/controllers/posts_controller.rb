d61689885f39e1f0c91a50b3e5a97a87c32b3c18
Shoko Ishigaki <shoko.ishigaki@gmail.com>
2023-09-02 20:56:16 +0900

    LINE ID未設定の場合の処理追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,28 @@
 class PostsController < ApplicationController
     def create
-        data = {id_token: post_params[:line_id_token], client_id: '2000560013'}
-        uri = URI.parse("https://api.line.me/oauth2/v2.1/verify")
-        response = Net::HTTP.post_form(uri, data)
-        puts JSON.parse(response.body)
-        puts response.code
-        if response.code == "200"
-            response_json = JSON.parse(response.body)
-            line_id = response_json["sub"]
-            if !User.exists?(line_id: line_id)
-                user = User.new(line_id: line_id, name: response_json["name"])
-                user.save
+        if !post_params[:line_id_token].nil?
+            data = {id_token: post_params[:line_id_token], client_id: '2000560013'}
+            uri = URI.parse("https://api.line.me/oauth2/v2.1/verify")
+            response = Net::HTTP.post_form(uri, data)
+            puts JSON.parse(response.body)
+            puts response.code
+            if response.code == "200"
+                response_json = JSON.parse(response.body)
+                line_id = response_json["sub"]
+                if !User.exists?(line_id: line_id)
+                    user = User.new(line_id: line_id, name: response_json["name"])
+                    user.save
+                end
+            else
+                response_internal_server_error
             end
-            post = Post.new(title: post_params[:title], text_body: post_params[:text_body], line_id: line_id)
-            post.save
-            response_success(:post, :create)
         else
-            response_internal_server_error
+            line_id = nil
         end
 
-        # @user = User.new(user_params)
-        # if @user.name.blank?
-        #   # 必須パラメータが欠けている場合
-        #   response_bad_request
-        # else
-        #   if User.exists?(email: @user.email)
-        #     # 既に登録済みのメールアドレスで登録しようとした場合
-        #     response_conflict(:user)
-        #   else
-        #     if @user.save
-        #       # ユーザ登録成功
-        #       response_success(:user, :create)
-        #     else
-        #       # 何らかの理由で失敗
-        #       response_internal_server_error
-        #     end
-        #   end
-        # end
+        post = Post.new(title: post_params[:title], text_body: post_params[:text_body], line_id: line_id)
+        post.save
+        response_success(:post, :create)
     end
 
     def post_params
